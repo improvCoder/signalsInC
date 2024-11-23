f1ee715405e0aa5be9ac94e85cdd9f1e08094f22
improvCoder <166443448+improvCoder@users.noreply.github.com>
2024-11-22 22:15:15 -0800

    Add files via upload
    displaying keyboard interrupts

---
+++ pointers.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <csignal>
+using namespace std;
+
+void signal_handler(int signal_num)
+{
+  cout<<"Signal Number is:"<<signal_num<<endl;
+  exit(signal_num);
+}
+
+
+int main()
+{
+  signal(SIGINT,signal_handler);
+  while(true)
+    {
+      cout<<"hello world"<<endl;
+    }
+  return 0;
+}
+
+
+
+
+
+
+
